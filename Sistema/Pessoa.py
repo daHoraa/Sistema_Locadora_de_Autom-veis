c446da4e5f9314ee05c2ac9a3d8b76b47c2692ba
Karen Giovanna Melo <49700354+karengiovanna@users.noreply.github.com>
2019-10-12 16:46:48 -0400

    Fiz herança básica utilizando a classe Endereço

--- Sistema/Pessoa.py
+++ Sistema/Pessoa.py
@@ -1,6 +1,10 @@
-class Pessoa:
-  def __init__(self, nome, rg, cpf, nascimento, estado_civil, sexo):
+class Pessoa(Endereco):
+  def __init__(self, nome, rg, cpf, nascimento, estado_civil, sexo, CEP, rua, bairro, n_casa, cidade, estado):
     self.nome = nome
     self.rg = rg
     self.nascimento = nascimento
-    self.estado_civil
+    self.estado_civil = estado_civil
+    self.sexo = sexo
+    super().__init__(CEP, rua, bairro, n_casa, cidade, estado)
+ 
+    
