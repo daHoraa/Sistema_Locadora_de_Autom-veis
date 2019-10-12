0d64a5a5fddbbe214f447da00ed9f1d335df94c9
Karen Giovanna Melo <49700354+karengiovanna@users.noreply.github.com>
2019-10-12 16:42:21 -0400

    Criei a classe endereço e adicionei atributos

---
+++ Sistema/Endereco.py
@@ -0,0 +1,10 @@
+class Endereço:
+  def __init__(self, CEP, rua, bairro, n_casa, cidade, estado):
+    self.CEP = CEP
+    self.rua = rua
+    self.bairro = bairro
+    self.n_casa = n_casa
+    self.cidade = cidade
+    self.estado = estado
+    
+  # criar metodo trocar endereço
