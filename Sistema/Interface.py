f3c308d2dce1c1a7ba0c4c104e0007a166f443ab
Karen <giovannagiovanna13@gmail.com>
2019-10-12 20:06:36 -0400

    Editei a aparência da aplicação

--- Sistema/Interface.py
+++ Sistema/Interface.py
@@ -19,18 +19,38 @@ class Interface:
     def __init__(self, master=None):
         pass
 
-root = Tk()
+    #Método verificar senha
+    def verificaSenha(self):
+        usuario = self.nome.get()
+        senha = self.senha.get()
+        if usuario == "usuario" and senha == "123":
+            self.mensagem["text"] = "Autenticado"
+        else:
+            self.mensagem["text"] = "Erro na autenticação"
+
+janela = Tk()
 '''
-instanciamos a classe TK() através da variável root, que foi criada no final do código.
+instanciamos a classe TK() através da variável janela, que foi criada no final do código.
 Essa classe permite que os widgets possam ser utilizados na aplicação.
 '''
-Application(root)
+Interface(janela)
 '''
-passamos a variável root como parâmetro do método
+passamos a variável janela como parâmetro do método
 construtor da classe Application. 
 '''
-root.mainloop()
+#altera o titulo da janela
+janela.title("Locadora 4x4")
+
+#altera a cor do fundo
+janela["bg"] = "pink"
+
+#LARGURAxALTURA + ESQUERDA + DISTANCIA DO TOPO
+#300X300+100+100
+janela.geometry("1000x500+100+100")
+
+janela.mainloop()
 '''
-chamamos o método root.mainloop() para exibirmos a tela.
+chamamos o método janela.mainloop() para exibirmos a tela.
 Sem o event loop, a interface não será exibida.
+Interrompe o script enquanto a janela principal estiver aberta
 '''
