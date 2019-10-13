b253a23170e9a6e48ad6247f384597d316aa6e25
Karen Giovanna Melo <49700354+karengiovanna@users.noreply.github.com>
2019-10-12 20:49:32 -0400

    Update and rename Pessoa.py to pessoa.py

--- Sistema/pessoa.py
+++ Sistema/pessoa.py
@@ -1,3 +1,5 @@
+from endereco import *
+
 class Pessoa(Endereco):
   def __init__(self, nome, rg, cpf, nascimento, estado_civil, sexo, CEP, rua, bairro, n_casa, cidade, estado):
     self.nome = nome
