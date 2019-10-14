70622b35f04a1da4e3c51d1afd09e2e1b631c642
Karen <giovannagiovanna13@gmail.com>
2019-10-14 11:24:05 -0400

    Update

---
+++ Sistema/veiculo.py
@@ -0,0 +1,17 @@
+
+class Veiculo:
+    def __init__(self, tipo, marca, modelo, ano, cor, tanque, combustivel, consumo_cidade, consumo_estrada, tempo_0_100, chassi, placa, tamanho_pneu, preco ):
+         self.tipo=tipo #carro ou moto
+         self.marca=marca
+         self.modelo=modelo
+         self.ano=ano
+         self.cor = cor
+         self.tanque = tanque
+         self.combustivel = combustivel
+         self.consumo_cidade=consumo_cidade
+         self.consumo_estrada=consumo_estrada
+         self.tempo_0_100=tempo_0_100
+         self.chassi=chassi
+         self.placa=placa
+         self.tamanho_pneu =tamanho_pneu 
+         self.preco=preco
