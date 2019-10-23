31e6efe0ac2bfd3f6d4e35b0780323614942dc80
Karen <giovannagiovanna13@gmail.com>
2019-10-23 11:44:36 -0400

    Adicionei setters e getters

--- Sistema/pessoa.py
+++ Sistema/pessoa.py
@@ -2,12 +2,22 @@ from endereco import *
 
 class Pessoa(Endereco):
   def __init__(self, nome, rg, cpf, nascimento, estado_civil, sexo, CEP, rua, bairro, n_casa, cidade, estado):
-    self.nome = nome
-    self.rg = rg
-    self.nascimento = nascimento
-    self.estado_civil = estado_civil
-    self.sexo = sexo
+    self._nome = nome
+    self._rg = rg
+    self._nascimento = nascimento
+    self._estado_civil = estado_civil
+    self._sexo = sexo
     super().__init__(CEP, rua, bairro, n_casa, cidade, estado)
   
- 
-    
+ def _set_nome(self,nome):
+      if not isinstance(nome, str):
+        raise TypeError("Nome: entre com uma string")
+      if len(nome < 3):
+        raise TypeError("Nome: entre com seu nome completo")
+      self._nome = nome;
+
+  def _get_nome(self):
+        self._nome = nome
+  
+  def _set_cpf(self.cpf):
+        if cpf
