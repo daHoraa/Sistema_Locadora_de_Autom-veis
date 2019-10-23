ab50269eaeb6ad7a4b8fa56c527bb08c0c0f5100
Karen <giovannagiovanna13@gmail.com>
2019-10-23 11:45:31 -0400

    update

--- Sistema/Interface.py
+++ Sistema/Interface.py
@@ -11,29 +11,49 @@ except ImportError:
 
 '''importamos todos os componentes do módulo Tkinter'''
 
-class Interface:
-    '''
-    Criamos uma classe chamada Application.
-    É nela que criaremos os controles que serão exibidos na tela.
-    '''
-    def __init__(self, master=None):
-        pass
+import tkinter.messagebox as tm
 
-    #Método verificar senha
-    def verificaSenha(self):
-        usuario = self.nome.get()
-        senha = self.senha.get()
-        if usuario == "usuario" and senha == "123":
-            self.mensagem["text"] = "Autenticado"
+
+class LoginFrame(Frame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.label_username = Label(self, text="Username")
+        self.label_password = Label(self, text="Password")
+
+        self.entry_username = Entry(self)
+        self.entry_password = Entry(self, show="*")
+
+        self.label_username.grid(row=0, sticky=E)
+        self.label_password.grid(row=1, sticky=E)
+        self.entry_username.grid(row=0, column=1)
+        self.entry_password.grid(row=1, column=1)
+
+        self.checkbox = Checkbutton(self, text="Keep me logged in")
+        self.checkbox.grid(columnspan=2)
+
+        self.logbtn = Button(self, text="Login", command=self._login_btn_clicked)
+        self.logbtn.grid(columnspan=2)
+
+        self.pack()
+
+    def _login_btn_clicked(self):
+        # print("Clicked")
+        username = self.entry_username.get()
+        password = self.entry_password.get()
+
+        # print(username, password)
+
+        if username == "john" and password == "password":
+            tm.showinfo("Login info", "Welcome John")
         else:
-            self.mensagem["text"] = "Erro na autenticação"
-
+            tm.showerror("Login error", "Incorrect username")
 janela = Tk()
 '''
 instanciamos a classe TK() através da variável janela, que foi criada no final do código.
 Essa classe permite que os widgets possam ser utilizados na aplicação.
 '''
-Interface(janela)
+LoginFrame(janela)
 '''
 passamos a variável janela como parâmetro do método
 construtor da classe Application. 
