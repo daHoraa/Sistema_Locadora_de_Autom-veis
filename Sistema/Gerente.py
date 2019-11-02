1690a4cf036d20ad63f3ff7fa1e30e75b7c90df6
João Lucas Sidney Rodrigues <43157844+JaoLucass@users.noreply.github.com>
2019-11-02 15:35:01 -0400

    Add files via upload
    classe Gerente criada

---
+++ Sistema/Gerente.py
@@ -0,0 +1,19 @@
+from pessoa import *
+from veiculo import *
+
+class Gerente:
+
+    def cria_funcionario():
+        pass
+
+    def editar_funcionario():
+        pass
+
+    def remover_cliente():
+        pass
+
+    def inserir_veiculo():
+        pass
+
+    def remover_veiculo():
+        pass
