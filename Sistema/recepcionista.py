25c6c6f43be80b8621b3ab815e673d346c30a28d
daHoraa <tiagohora87@gmail.com>
2019-11-03 14:39:07 -0400

    prototipos dos metodos

---
+++ Sistema/recepcionista.py
@@ -0,0 +1,21 @@
+from Funcionario import*
+
+class Recepcionista(Funcionario):
+    def cadastrar_cliente():
+
+    def verificar_cliente():
+
+    def avaliar_cliente():
+
+    def emitir_contrato():
+
+    def locar_veiculo():
+
+    def devolver_veiculo():
+
+    def verificar_disponibilidade():
+
+    def efetuar_pagamento():
+
+    def agendar_manutencao():
+        
