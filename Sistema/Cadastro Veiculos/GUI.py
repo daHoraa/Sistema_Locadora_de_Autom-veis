0e62844fcd3b403dced671cf31b5269d15fbbbea
Karen <giovannagiovanna13@gmail.com>
2019-11-09 10:46:09 -0400

    Correção no banco de dados Clientes

--- Sistema/Cadastro Veiculos/GUI.py
+++ Sistema/Cadastro Veiculos/GUI.py
@@ -166,4 +166,4 @@ class Gui():
         self.configure_sizes()
         self.window.mainloop()
 
-                                                                                                        Gui().run()
+#Gui().run()
