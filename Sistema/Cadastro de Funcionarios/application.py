475984b35c929a970123a79554838693dd605472
João Lucas <JoaoLucaSidney@outlook.com>
2019-11-13 17:20:26 -0400

    application.py e dao.py com problema de instanceamento de algumas classes e métodos

---
+++ Sistema/Cadastro de Funcionarios/application.py
@@ -0,0 +1,107 @@
+from GUI_Dados_Funcionario import *
+from dao import FuncionarioDAO
+from tkinter import END
+from funcionario import Funcionario
+
+class Controller:
+    def __init__(self):
+        self.gui = Gui()
+        self.dao = FuncionarioDAO()
+        self.selected = None #funcionario selecionado
+        self.currentFuncionario = Funcionario()
+
+    def view_command(self):
+        "método para visualização dos resultados"
+        try:
+            rows = self.dao.view()
+            self.gui.listFuncionarios.delete(0, END)
+            for r in rows:
+                self.gui.listFuncionarios.insert(END, r)
+        except Exception as e:
+            print(e)
+
+    def __fill_current_funcionario(self):
+
+        self.currentFuncionarioNacionalidade = self.gui.txtNacionalidade.get()
+        self.currentFuncionarioTipo_documento = self.gui.txtTipo_Documento.get()
+        self.currentFuncionarioCpf = self.gui.txtCPF.get()
+        self.currentFuncionarioNome = self.gui.txtNome.get()
+        self.currentFuncionarioSobrenome = self.gui.txtSobrenome.get()
+        self.currentFuncionarioEmail = self.gui.txtEmail.get()
+        self.currentFuncionarioGenero = self.gui.txtGenero.get()
+        self.currentFuncionarioTelefone = self.gui.txtTelefone.get()
+        self.currentFuncionarioNascimento = self.gui.txtNascimento.get()
+        self.currentFuncionarioInstrucao = self.gui.txtInstrucao.get()
+
+    def search_command(self):
+        self.gui.listFuncionarios.delete(0, END)
+        self.__fill_current_funcionario()
+        try:
+            rows = self.dao.search(self.currentFuncionario)
+            for r in rows:
+                self.gui.listFuncionarios.insert(END, r)
+        except Exception as e:
+            print(e)
+
+    def insert_command(self):
+        "método para inserir registros"
+        self.__fill_current_Funcionario()
+        self.dao.insert(self.currentFuncionario)
+        self.view_command()
+
+    def get_selected_row(self, event):
+        "método que seleciona na listbox e popula os campos de input"
+        if self.gui.listFuncionarios.curselection():
+            index = self.gui.listFuncionarios.curselection()[0]        
+            self.selected = self.gui.listFuncionarios.get(index)
+            
+            self.gui.entNacionalidade.delete(0, END)
+            self.gui.entNacionalidade.insert(END, self.selected[1])
+            self.gui.entTipo_Documento.delete(0, END)
+            self.gui.entTipo_Documento.insert(END, self.selected[1])
+            self.gui.entCPF.delete(0, END)
+            self.gui.entCPF.insert(END, self.selected[4])
+            self.gui.entNome.delete(0, END)
+            self.gui.entNome.insert(END, self.selected[1])
+            self.gui.entSobrenome.delete(0, END)
+            self.gui.entSobrenome.insert(END, self.selected[2])
+            self.gui.entEmail.delete(0, END)
+            self.gui.entEmail.insert(END, self.selected[3])
+            self.gui.entGenero.delete(0, END)
+            self.gui.entGenero.insert(END, self.selected[4])
+            self.gui.entTelefone.delete(0, END)
+            self.gui.entTelefone.insert(END, self.selected[4])
+            self.gui.entNascimento.delete(0, END)
+            self.gui.entNascimento.insert(END, self.selected[1])
+            self.gui.entInstrucao.delete(0, END)
+            self.gui.entInstrucao.insert(END, self.selected[2])
+
+    def update_command(self):
+        "método para atualizar registro"
+        id = self.selected[0]
+        self.__fill_current_Funcionario()
+        self.dao.update(id,self.currentFuncionario)
+        self.view_command()
+
+    def del_command(self):
+        "método para remover registro"
+        id = self.selected[0]
+        self.dao.delete(id)
+        self.view_command()
+
+    def close_command(self):
+        self.dao.close()
+        self.gui.window.destroy()
+
+    def start(self):
+        self.gui.listFuncionarios.bind('<<ListboxSelect>>', self.get_selected_row)
+        #associando o comportamento à interface
+        self.gui.btnViewAll.configure(command=self.view_command)
+        self.gui.btnBuscar.configure(command=self.search_command)
+        self.gui.btnInserir.configure(command=self.insert_command)
+        self.gui.btnUpdate.configure(command=self.update_command)
+        self.gui.btnDel.configure(command=self.del_command)
+        self.gui.btnClose.configure(command=self.close_command)
+        self.gui.run()
+
+Controller().start()
--- Sistema/Cadastro de Funcionarios/dao.py
+++ Sistema/Cadastro de Funcionarios/dao.py
@@ -35,7 +35,7 @@ class BdAccess():
         else:
             return False
 
-class ClientDAO:
+class FuncionarioDAO:
     def __init__(self):
         "Quando a aplicação for executada pela primeira vez, cria-se o banco de dados"
         self.bd = BdAccess()
@@ -46,7 +46,7 @@ class ClientDAO:
         "recupera todos os dados do banco."
         rows = None
         try:
-            self.bd.execute("SELECT * FROM clientes")    
+            self.bd.execute("SELECT * FROM funcionarios")    
             rows = self.bd.fetchall()
         except sql.Error as error:
             print("Falha ao tentar selecionar os registros")
@@ -58,10 +58,46 @@ class ClientDAO:
     def insert(self, cliente):
         "insere novos registros no banco"
         try:
-            self.bd.execute("INSERT INTO clientes VALUES(NULL, ?, ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?"), ( self.nacionalidade , self.tipo_domumento , self.cpf , self.nome , self.sobrenome , self.email , self.cpf , self.genero , self.telefone, self.rg , self.nascimento , self.cep , self.logradouro , self.endereco , self.numero_endereco , self.complemento , self.bairro , self.cidade , self.estado))
+            self.bd.execute("INSERT INTO funcionarios VALUES(NULL, ?, ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?", ( self.nacionalidade , self.tipo_domumento , self.cpf , self.nome , self.sobrenome , self.email , self.cpf , self.genero , self.telefone, self.rg , self.nascimento , self.cep , self.logradouro , self.endereco , self.numero_endereco , self.complemento , self.bairro , self.cidade , self.estado))
             self.bd.persist()
         except sqlite3.Error as error:
             print("Falha ao tentar inserir os registros")
             print("Classe da exceção: ", error.__class__)
             print("Exceção é ", error.args)
-        
+    
+    def search(self, cliente):
+        rows = None
+        try:
+            self.bd.execute("SELECT * FROM funcionarios WHERE nacionalidade =? or tipo_documento =? or nome=? or sobrenome=? or email=? or cpf=? or cpf =? or genero =? or telefone =? or numero_cnh =? or numero_registro_cnh =? or data_validade_cnh =? or uf_cnh =? or rg =? or nascimento =? or cep = ? or logradouro = ? or endereco = ? or numero_endereco = ? or complemento = ? or bairro = ? or cidade = ? or estado = ?", (cliente.nacionalidade or cliente.tipo_domumento or cliente.nome or cliente.sobrenome or cliente.email or cliente.cpf or cliente.genero or cliente.telefone or cliente.numero_cnh or cliente.numero_registro_cnh or cliente.data_validade_cnh or cliente.uf_cnh or cliente.rg or cliente.nascimento or cliente.cep or cliente.logradouro or cliente.endereco or cliente.numero_endereco or cliente.complemento or cliente.bairro or cliente.cidade or cliente.estado))
+            rows = self.bd.fetchall()
+        except sqlite3.Error as error:
+            print("Falha ao tentar buscar os registros")
+            print("Classe da exceção: ", error.__class__)
+            print("Exceção é ", error.args)
+            raise Exception
+        return rows
+
+    def update(self, id, cliente):
+        "atualiza registros no banco"
+        try:
+            self.bd.execute("UPDATE funcionarios SET nacionalidade =? ,tipo_domumento =? ,cpf =? ,nome =? ,sobrenome =? ,email =? ,cpf =? ,genero =? ,telefone =? ,numero_cnh =? ,numero_registro_cnh =? ,data_validade_cnh =? ,uf_cnh =? ,rg =? ,nascimento =? ,cep =? ,logradouro =? ,endereco =? ,numero_endereco =? ,complemento =? ,bairro =? ,cidade =? ,estado =? WHERE id = ?", (cliente.nacionalidade    , cliente.tipo_domumento    , cliente.cpf    , cliente.nome    , cliente.sobrenome    , cliente.email    , cliente.cpf    , cliente.genero    , cliente.telefone    , cliente.numero_cnh    , cliente.numero_registro_cnh    , cliente.data_validade_cnh    , cliente.uf_cnh    , cliente.rg    , cliente.nascimento    , cliente.cep    , cliente.logradouro    , cliente.endereco    , cliente.numero_endereco    , cliente.complemento    , cliente.bairro    , cliente.cidade    , cliente.estado , id))
+            self.bd.persist()
+        except sqlite3.Error as error:
+            print("Falha ao tentar atualizar os registros")
+            print("Classe da exceção: ", error.__class__)
+            print("Exceção é ", error.args)
+
+    def delete(self, id):
+        "remove registros no banco"
+        try:
+            self.bd.execute("DELETE FROM funcionarios WHERE id = ?", (id,))
+            self.bd.persist()
+        except sqlite3.Error as error:
+            print("Falha ao tentar remover o registro")
+            print("Classe da exceção: ", error.__class__)
+            print("Exceção é ", error.args)
+
+    def close(self):
+        "fechar o banco"
+        print('Fechando o Banco...')
+        self.bd.disconnect()
