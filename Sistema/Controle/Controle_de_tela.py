3d3cbdb6c5ca3a03379c8a92248b915ad79e7945
Karen <giovannagiovanna13@gmail.com>
2019-11-26 14:31:41 -0400

    Delete

--- Sistema/Controle/Controle_de_tela.py
+++
@@ -1,2 +0,0 @@
-import tkinter.messegebox
-from tkinter import END
--- Sistema/Telas/__init__.py
+++
@@ -1,5 +0,0 @@
-from .tela import Tela
-from .tela_CadastroFuncionario import Tela_CadastroFuncionario
-from .tela_CadastroVeiculos import Tela_CadastroVeiculos
-from .tela_DadosPessoais import Tela_DadosPessoais
-from .tela_Entrada import Tela_Entrada
