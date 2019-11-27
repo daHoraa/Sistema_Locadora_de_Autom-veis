04f6637524de6cf0aae043e80831c92bfb18879d
Karen <giovannagiovanna13@gmail.com>
2019-11-26 20:52:10 -0400

    Delete

--- SistemaLocadoraAutomoveis/locacao.py
+++
@@ -1,9 +0,0 @@
-
-class Locacao():
-    def __init__(self, id_cliente="", id_veiculo="", data_inicial="", data_final="", diarias="", valor_locacao=""):
-        # dados da cnh
-        self.id_cliente = id_cliente
-        self.id_veiculo = id_veiculo
-        self.data_inicial = data_inicial
-        self.diarias = diarias
-        self.valor_locacao = valor_locacao
